b77265c0c8f35ff86b7ddba4c81393df30cd5663
Sergey Seroukhov <seroukhov@gmail.com>
2020-01-14 13:13:45 -0700

    Minor changes

--- obj/src/build/CreditCardsClientFactory.js
+++ obj/src/build/CreditCardsClientFactory.js
@@ -13,9 +13,9 @@ class CreditCardsClientFactory extends pip_services3_components_node_1.Factory {
         this.registerAsType(CreditCardsClientFactory.LambdaClientV1Descriptor, CreditCardsLambdaClientV1_1.CreditCardsLambdaClientV1);
     }
 }
+exports.CreditCardsClientFactory = CreditCardsClientFactory;
 CreditCardsClientFactory.Descriptor = new pip_services3_commons_node_1.Descriptor('pip-services-creditcards', 'factory', 'default', 'default', '1.0');
 CreditCardsClientFactory.DirectClientV1Descriptor = new pip_services3_commons_node_1.Descriptor('pip-services-creditcards', 'client', 'direct', 'default', '1.0');
 CreditCardsClientFactory.HttpClientV1Descriptor = new pip_services3_commons_node_1.Descriptor('pip-services-creditcards', 'client', 'http', 'default', '1.0');
 CreditCardsClientFactory.LambdaClientV1Descriptor = new pip_services3_commons_node_1.Descriptor('pip-services-creditcards', 'client', 'lambda', 'default', '1.0');
-exports.CreditCardsClientFactory = CreditCardsClientFactory;
 //# sourceMappingURL=CreditCardsClientFactory.js.map
